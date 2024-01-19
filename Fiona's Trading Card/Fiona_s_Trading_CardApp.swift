3b34f689d6def76ed7b0b66c4ccb76ad3d158bd4
Xinlin Zhou <XZhou@lcs.on.ca>
2024-01-19 17:48:40 -0500

    Initial Commit

---
+++ Fiona's Trading Card/Fiona_s_Trading_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fiona_s_Trading_CardApp.swift
+//  Fiona's Trading Card
+//
+//  Created by Fiona ZHOU on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct Fiona_s_Trading_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
